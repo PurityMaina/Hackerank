c977b256bf95b44b22d60f7f5adebfcc139a14b8
Purity Maina <puritymaina001@gmail.com>
2019-08-21 07:55:38 +0300

    the clouds do not jump optimally

--- jumping_on_clouds.rb
+++ jumping_on_clouds.rb
@@ -5,22 +5,24 @@ require 'stringio'
 
 # Complete the jumpingOnClouds function below.
 def jumpingOnClouds(c)
-#loop through array, if 1 skip 
-#deermine minimum jumps?
-skip =  0
-    c.each do |cloud|
-        if cloud == 1
-            puts "cloud is Thundercloud"
-            skip += 1
-        else
-            puts "cloud is cloud"
-        end
-    end
-    puts "Total Thunderclouds ",skip
-        puts "Total Cumulus clouds  ",c.size - (skip+1)
-
-    return c.size - (skip+1) 
+#get current position, check if cumulus
+#Jump to the next cumulus cloud equal to the number of the current cloud plus 2.
+#If Thundercloud, skip i.e of the current cloud plus 1. 
+n = c.length
+clouds = 0
+pos = 0
+jumps = 0
+while pos < n
+  if clouds[pos + 2] == 0
+    pos += 2
+  else
+    pos += 1
+  end
+  jumps += 1
 end
+return jumps
+end
+
 fptr = File.open(ENV['OUTPUT_PATH'], 'w')
 
 n = gets.to_i
@@ -33,24 +35,3 @@ fptr.write result
 fptr.write "\n"
 
 fptr.close()
-
-
-def jumpingOnClouds(c)
-#get current position, check if cumulus
-#Jump to the next cumulus cloud equal to the number of the current cloud plus 2.
-#If Thundercloud, skip i.e of the current cloud plus 1. 
-
-skip =  0
-position = 0
-n = gets.chomp.to_i
-clouds = gets.chomp.split(' ').map(&:to_i)
-
-    while position < n
-          if clouds[position + 2] == 0
-          position +=2
-        else
-            pos += 1        
-        end
-    skip += 1
-    end
-end
